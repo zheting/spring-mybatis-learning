aa456246b102cc121f6999988c19f5b82fe1518b
devin12 <1400902533@qq.com>
2020-12-25 07:41:18 +0800

    hello world

--- src/test/java/cn/java/money/mybatis/test/MyBatisTest.java
+++ src/test/java/cn/java/money/mybatis/test/MyBatisTest.java
@@ -28,14 +28,29 @@ public class MyBatisTest {
         sqlSession.close();
     }
 
+    /*
+     *  SqlSession 代表和数据库的一次会话，用完必须关闭
+     *  SqlSession 和 Connection 一样都是非线程安全的。每次使用都应该去获取新的对象。
+     *  mapper接口没有实现类，但是mybatis会为这个接口生成一个代理对象
+     *      EmployeeMapper mapper = sqlSession.getMapper(EmployeeMapper.class);
+     */
+
     @Test
     public void test2() throws IOException {
         String resource = "mybatis/mybatis-config.xml";
         InputStream inputStream = Resources.getResourceAsStream(resource);
         SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
 
+       /* DataSource dataSource = new PooledDataSource( "com.mysql.cj.jdbc.Driver","jdbc:mysql://localhost:3306/mybatis?characterEncoding=UTF-8&amp;&amp;serverTimezone=GMT","root","123456");
+        TransactionFactory transactionFactory = new JdbcTransactionFactory();
+        Environment environment = new Environment("development", transactionFactory, dataSource);
+        Configuration configuration = new Configuration(environment);
+        configuration.addMapper(EmployeeMapper.class);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(configuration);*/
+
+
         SqlSession sqlSession = sqlSessionFactory.openSession();
-        // 获取接口的实现对象
+        // 将xml和接口绑定， 获取接口的实现对象
         EmployeeMapper mapper = sqlSession.getMapper(EmployeeMapper.class);
         Employee employee = mapper.getEmployeeById(1);
         System.out.println(employee);
